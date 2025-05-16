8767f498c2b3349ebef4316d5b4c1873029e6379
FutureIntent <IamSoulfuller@gmail.com>
2025-05-16 19:53:28 +0300

    Inital Docker setup

---
+++ Java/spaAuth/src/main/java/com/app/spaAuth/SpaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.app.spaAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaAuthApplication.class, args);
+	}
+
+}
---
+++ Java/spaAuth/src/test/java/com/app/spaAuth/SpaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.spaAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
